36978a6d282f968a48065153bd878a5545080e99
GayaniGunasekara <gayanigunasekara2001@gmail.com>
2025-05-21 15:38:57 +0530

    Has connected Firebase and Flutter

--- MobileApp/test/widget_test.dart
+++ MobileApp/test/widget_test.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:notification_interface/main.dart';
 
 void main() {
   testWidgets('Notification titles are displayed', (WidgetTester tester) async {
-    await tester.pumpWidget(NotificationApp());
+    await tester.pumpWidget(const MyApp());
 
     expect(find.text('Elephants near the fence!'), findsOneWidget);
     expect(find.text('Electricity issues in the fence.'), findsOneWidget);
