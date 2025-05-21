0d22cc358b0e76134a7ca28d803675496497858f
Kaumadii <162365196+Kaumadii@users.noreply.github.com>
2025-05-21 16:08:33 +0530

    connect EleGuardian app to Firebase

--- MobileApp/lib/main.dart
+++ MobileApp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:health_app/pages/home_page.dart';
@@ -5,7 +6,9 @@ import 'package:health_app/pages/home_page.dart';
 // Import our model
 import 'pages/user_model.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     ChangeNotifierProvider(
       create: (context) => UserProvider(),
