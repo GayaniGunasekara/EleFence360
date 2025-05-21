a995f4b28e40d394b3753cab45bf6e0026957350
Kaumadii <162365196+Kaumadii@users.noreply.github.com>
2025-05-21 13:37:15 +0530

    Add EleGuardian home page and user profile page for elephant detection system

---
+++ MobileApp/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:health_app/pages/home_page.dart';
+
+// Import our model
+import 'pages/user_model.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Health App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color(0xFF1E3A64),
+        // Removed fontFamily: 'Roboto'
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
